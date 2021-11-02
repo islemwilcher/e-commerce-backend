dc4275a8f5291a6f16e37cc5ac8127f6755acfde
islemwilcher <islemadobe6@gmail.com>
2021-11-02 03:40:26 +0100

    adding the order model and editing the prodect and user model

--- moduls/products.js
+++ moduls/products.js
@@ -1,14 +1,17 @@
 
 import mongoose from 'mongoose'
 
-const productSchema = mongoose.Schema({
-    name:  { type: String, required: true, unique: true },
-    img: { type: String, required: true },
-    description: { type: String, required: true },
-    category: { type: Array },
-    color: { type: Array },
-    price:  { type: Number, required: true },
-    inStock: { type: Boolean, default: true }
-})
+const productSchema = mongoose.Schema(
+    {
+        name:  { type: String, required: true, unique: true },
+        img: { type: String, required: true },
+        description: { type: String, required: true },
+        category: { type: Array },
+        color: { type: Array },
+        price:  { type: Number, required: true },
+        inStock: { type: Boolean, default: true }
+    },  
+    { timestapms: true }
+)
 
 export default mongoose.model('Product', productSchema)
