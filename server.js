e054eb19caf687d947b1e85e5ddc2713a3fa5845
islemwilcher <islemadobe6@gmail.com>
2021-11-02 02:13:09 +0100

    adding the server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+app.use(cors())
+
+const PORT = process.env.PORT|| 5000
+
+app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`))
