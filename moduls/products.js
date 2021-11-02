30acd21cc95dae54e7376bdcbd8eafa973d7d4e7
islemwilcher <islemadobe6@gmail.com>
2021-11-02 03:25:56 +0100

    adding the product schema

---
+++ moduls/products.js
@@ -0,0 +1,14 @@
+
+import mongoose from 'mongoose'
+
+const productSchema = mongoose.Schema({
+    name:  { type: String, required: true, unique: true },
+    img: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: Array },
+    color: { type: Array },
+    price:  { type: Number, required: true },
+    inStock: { type: Boolean, default: true }
+})
+
+export default mongoose.model('Product', productSchema)
