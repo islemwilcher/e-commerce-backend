2ff4d15375a694cff651005b4d1ba7e8b4204426
islemwilcher <islemadobe6@gmail.com>
2021-11-02 02:49:02 +0100

    making the connection to the database

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
+
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -13,4 +16,8 @@ app.use(cors())
 
 const PORT = process.env.PORT|| 5000
 
-app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`))
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT} and db connected`)))
+    .catch((error) => console.log(`${error} did not connect`));
+
+
