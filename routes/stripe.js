0303d68cc600f1f543b7d094c9097d6fd140f6d3
islemwilcher <islemadobe6@gmail.com>
2021-11-24 19:52:26 +0100

    #

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,11 +1,23 @@
 
-import express from "express"
-
-
-import { payment } from "../controllers/stripe.js"
+import express from 'express'
+import Stripe from 'stripe'
+const stripe = Stripe(process.env.STRIPE_KEY)
 
 const router = express.Router()
 
-router.post('/payment', payment)
-
-export default router
+export default router.post("/payment", (req, res) => {
+    stripe.charges.create(
+      {
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: "usd",
+      },
+      (stripeErr, stripeRes) => {
+        if (stripeErr) {
+          res.status(500).json(stripeErr);
+        } else {
+          res.status(200).json(stripeRes);
+        }
+      }
+    );
+  });
